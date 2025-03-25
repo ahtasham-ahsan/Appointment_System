2eb375bce939eccd7d2c9abd3c6127146abe50a8
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-03-25 11:47:20 +0500

    Database Configrations

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        });
+        }
+    );
         console.log('MongoDB Connected');
     } catch (error) {
         console.error('MongoDB Connection Failed:', error);
