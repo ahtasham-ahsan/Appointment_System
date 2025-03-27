8f9370133f0065cd80f5d6c2665bb48d130405b5
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-03-27 13:44:33 +0500

    Users Model

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    timezone: { type: String, required: true, default: 'UTC' } // Default to UTC
+    timezone: { type: String, required: true, default: 'UTC' } 
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
